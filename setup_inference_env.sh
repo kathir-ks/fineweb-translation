5d761810b4505cc43c70a195ade599f5975dd062
kathir <kathirksw@gmail.com>
2024-07-09 23:52:37 +0530

    fix typo in gcsfs

--- setup_inference_env.sh
+++ setup_inference_env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 pip install -U "jax[tpu]" -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
-pip install torch transformers datasets flax nltk fsspec gcsfc
+pip install torch transformers datasets flax nltk fsspec gcsfs
 sudo apt-get install golang -y
 pip install jax-smi
 

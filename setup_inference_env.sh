6f3827619a1376fee73b4e982c6f650d2e7a3926
kathir <kathirksw@gmail.com>
2024-08-11 15:46:41 +0530

    Add torch cpu only in env setup

--- setup_inference_env.sh
+++ setup_inference_env.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+pip install networkx==2.5
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
 pip install -U "jax[tpu]" -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 pip install torch transformers datasets flax nltk fsspec gcsfs
 sudo apt-get install golang -y
